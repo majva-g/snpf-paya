8dca6bdb5c5d90ced21f3cd0c9ba97e02cfb6adb
m.vahidkhoo <m.vahidkhoo@gmail.com>
2025-01-05 14:37:53 +0330

    feat (migration) : create transactions migration

---
+++ database/migrations/2024_12_19_100523_create_transactions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sheba_request_id')->constrained()->onDelete('cascade');
+            $table->decimal('amount', 20, 0);
+            $table->string('transaction_type'); // 'debit' or 'refund' or 'credit'
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
