3e2821e73bf0bf935e65c761023306e7e45719e9
m.vahidkhoo <m.vahidkhoo@gmail.com>
2025-01-06 11:10:59 +0330

    refactor (sheba) : override newFactory in sheba model

--- src/Domain/Sheba/Models/Sheba.php
+++ src/Domain/Sheba/Models/Sheba.php
@@ -2,10 +2,11 @@
 
 namespace Domain\Sheba\Models;
 
-
+use Database\Factories\ShebaFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Domain\User\Models\User;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 final class Sheba extends Model
 {
@@ -17,7 +18,12 @@ final class Sheba extends Model
         'balance',
     ];
 
-    public function user()
+    protected static function newFactory(): ShebaFactory
+    {
+        return ShebaFactory::new();
+    }
+
+    public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
     }
