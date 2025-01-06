55e4aee7437ae743e8f69f1cfb0df5f112905aa7
m.vahidkhoo <m.vahidkhoo@gmail.com>
2025-01-06 15:39:53 +0330

    feat (routes) : add sheba get route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use Application\API\Sheba\Controllers\CreateShebaRequestController;
+use Application\API\Sheba\Controllers\GetShebaRequestsListController;
 use Illuminate\Support\Facades\Route;
 
 
+ Route::get('/sheba', GetShebaRequestsListController::class); 
  Route::post('/sheba', CreateShebaRequestController::class);
 
