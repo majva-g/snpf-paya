246fdb68e0ef8b022e239569180db04004b7f61e
m.vahidkhoo <m.vahidkhoo@gmail.com>
2025-01-06 10:48:26 +0330

    refactor (database) : determine model to sheba factory

--- database/factories/ShebaFactory.php
+++ database/factories/ShebaFactory.php
@@ -2,14 +2,16 @@
 
 namespace Database\Factories;
 
+use Domain\Sheba\Models\Sheba;
 use Domain\User\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sheba>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Sheba\Models\Sheba>
  */
 class ShebaFactory extends Factory
 {
+    protected $model = Sheba::class;
     /**
      * Define the model's default state.
      *
