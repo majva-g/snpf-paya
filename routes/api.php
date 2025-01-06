4dec0f2e88149ed105355cb82883ad9fa8cb1a08
m.vahidkhoo <m.vahidkhoo@gmail.com>
2025-01-06 10:53:19 +0330

    feat (routes) : add sheba post route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use Application\API\Sheba\Controllers\CreateShebaRequestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+ Route::post('/sheba', CreateShebaRequestController::class);
+
